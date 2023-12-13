378b06ac211c4c06f6c920c0b05975c1ee681127
Dutus244 <duynguyen24th@gmail.com>
2023-12-13 20:49:31 +0700

    Cavern map + mob

---
+++ Assets/Scripts/Cavern/Bloatedbane.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloatedbane : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    float horizontal;
+    public float speed;
+    Animator animator;
+    private Vector2 lookDirection = new Vector2(1, 0);
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Hit");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("Attack1");
+            StartCoroutine(Launch());
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Attack2");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetTrigger("Dead");
+            StartCoroutine(DestroyAfterDelay(2f));
+        }
+    }
+
+     void FixedUpdate()
+    {
+        Vector2 velocity = new Vector2(horizontal, 0) * speed;
+        rb2d.velocity = velocity;
+
+        Vector2 move = new Vector2(horizontal, 0);
+        if (!Mathf.Approximately(move.x, 0.0f))
+        {
+            lookDirection.Set(move.x, 0);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Speed", move.magnitude);
+    }
+
+    // Coroutine to destroy the object after a delay
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+    IEnumerator Launch()
+    {
+        yield return new WaitForSeconds(0.8f);
+        float yOffset = 0.7f; 
+        Vector2 spawnPosition = rb2d.position + new Vector2(lookDirection.x, 0 + yOffset);
+        GameObject bulletObject = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);;
+
+        BloatedbaneBullet bullet = bulletObject.GetComponent<BloatedbaneBullet>();
+        bullet.Launch(lookDirection, 50);
+    }
+}
