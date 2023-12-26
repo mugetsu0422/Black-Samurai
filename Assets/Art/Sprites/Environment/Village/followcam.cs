099f300a6f797e5e46c2e430153ea1ed1bfa7dca
St0nks ✧ Confused <59339408+nhatruong1009@users.noreply.github.com>
2023-12-26 22:28:34 +0700

    .

--- Assets/Art/Sprites/Environment/Village/followcam.cs
+++ Assets/Art/Sprites/Environment/Village/followcam.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class followcam : MonoBehaviour
 {
+    public GameObject player;
     public GameObject cam;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,9 @@ public class followcam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = player.transform.position;
+        pos.z = Camera.main.transform.position.z;
+        Camera.main.transform.position = pos;
         gameObject.transform.position = cam.transform.position ;
     }
 }
